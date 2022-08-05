8a90eb048a7aaafc51896c8b80fff23426315f20
Andrew Chen <ignalchemi777@gmail.com>
2022-08-05 13:44:42 -0700

    added doc, github, and quote commands

--- bot/StockBot.py
+++ bot/StockBot.py
@@ -1,8 +1,10 @@
 import discord
 import TOKEN
+from pandas_datareader import data as pdr
 from discord.ext import commands
-from discord import Guild
+from datetime import date
 
+date = date.today()
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='$', intents=intents)
 
@@ -14,10 +16,8 @@ async def on_ready():
 
 @bot.command()
 async def ping(ctx):
-    await ctx.send(f"`Latency: {round(bot.latency * 1000)}ms`")
+    await ctx.send(f'`Latency: {round(bot.latency * 1000)}ms`')
 
-
-# guild = Guild()
 
 @bot.command()
 async def list(ctx):
@@ -27,7 +27,25 @@ async def list(ctx):
         memberList += str(member) + "\n"
     memberList = f"""```c
 --- MEMBERS --- 
-{memberList}```"""
+{memberList}
+```"""
     await ctx.send(memberList)
+
+@bot.command()
+async def doc(ctx):
+    await ctx.send('`discord.py Documentation:`\nhttps://discordpy.readthedocs.io/en/stable/index.html')
+
+@bot.command()
+async def github(ctx):
+    await ctx.send('`Github:`\n https://github.com/alchemi7/StockBot')
+
+@bot.command()
+async def quote(ctx, stock):
+    try:
+        quote = pdr.get_data_yahoo(stock, date, date).round(2)
+        await ctx.send(f"```{stock.upper()}: \n{quote}```")
+    except Exception:
+        await ctx.send("`Quote could not be found`")
+
 
 bot.run(TOKEN.TOKEN)
