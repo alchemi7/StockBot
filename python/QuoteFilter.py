cc7f34f3f4279976868b5febaf4275533a40c86c
Andrew Chen <ignalchemi777@gmail.com>
2022-12-26 20:19:28 -0800

    updated

--- python/QuoteFilter.py
+++ python/QuoteFilter.py
@@ -10,15 +10,15 @@ def quoteFilter(quote, stock, date):
 
     for i in range(0, len(carryList)):
         item = carryList[i]
-        carryList[i] = "{:,}".format(float(item))
+        carryList[i] = "{:,}".format(int(item))
     return (f"""```lua
 {stock.upper()} -- [{date}]
-Recent: ${carryList[5]}
+Recent: {carryList[5]}
 
 ====================
-Open: ${carryList[2]}
-High: ${carryList[0]}
-Low: ${carryList[1]}
-Close: ${carryList[3]}
+Open: {carryList[2]}
+High: {carryList[0]}
+Low: {carryList[1]}
+Close: {carryList[3]}
 Trade Volume: {carryList[4]}
 ```""")
